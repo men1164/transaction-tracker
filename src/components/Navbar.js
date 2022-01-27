3203b6712df3a859c4edfa22e8221a052ae693be
men1164 <41180013+men1164@users.noreply.github.com>
2022-01-27 15:07:42 +0700

    simple log out

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,7 +1,10 @@
 import { Link } from 'react-router-dom'
+import useLogout from '../hooks/useLogout'
 import styles from './Navbar.module.css'
 
 export default function Navbar() {
+  const { logout } = useLogout()
+
   return (
     <nav className={styles.navbar}>
       <ul>
@@ -17,6 +20,9 @@ export default function Navbar() {
           <Link to="/signup">
             Signup
           </Link>
+        </li>
+        <li>
+          <button className="btn" onClick={logout}>Logout</button>
         </li>
       </ul>
     </nav>
